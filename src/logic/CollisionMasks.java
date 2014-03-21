c57a99e7e0049416828dc1d76df8f2cee8816c00
hottied <dohidog@gmail.com>
2014-03-21 09:55:19 -0600

    Update CollisionMasks.java

--- src/logic/CollisionMasks.java
+++ src/logic/CollisionMasks.java
@@ -7,6 +7,6 @@ public interface CollisionMasks
 
 	public Polygon playerShipCollisionMask = new Polygon(new float[] {0, 304, 0, 279, 95, 189, 117,144 , 175, 0  ,192, 0  , 249, 63 , 249, 144 , 271,  189 , 366 , 279, 366,304 });
 	public Polygon redInvaderCollisionMask = new Polygon(new float[] { 61, 337, 330, 337, 390, 160, 243, 0, 147, 0, 0, 160, 61, 337 });
-	public Polygon invaderShotCollisionMask = new Polygon(new float[] {});
-	public Polygon playerShotCollisionMask = new Polygon(new float[] {});
+	public Polygon invaderShotCollisionMask = new Polygon(new float[] {28,20, 3, 193, 28 ,248, 53, 193});
+	public Polygon playerShotCollisionMask = new Polygon(new float[] {0,0, 0, 16, 16, 16, 16, 0});
 }
