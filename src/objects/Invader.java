f72fa24534be82f7ea3353888ca60382953a2b67
hottied <dohidog@gmail.com>
2014-04-09 09:23:21 -0600

    Update Invader.java

--- src/objects/Invader.java
+++ src/objects/Invader.java
@@ -8,7 +8,9 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Rectangle;
 
 import util.Point2;
-
+/**
+ * Invader class 
+ **/ 
 public class Invader implements Constants
 {
 
@@ -24,59 +26,103 @@ public class Invader implements Constants
 		this.health = health;
 		this.texture = texture;
 	}
-
+	/**
+	 *draws the invader ship
+	 *@param g - Graphics object
+	 **/ 
 	public void draw(final Graphics g)
 	{
 		g.texture(new Rectangle(location.getX(), location.getY(), size, size), texture, true);
 
 	}
+	/**
+	 * gets the health value of the invader
+	 * @return amount of health of the invader
+	 **/
 
 	public int getHealth()
 	{
 		return health;
 	}
+	/**
+	 * gets the size of the invader
+	 * @return size of the invader
+	 **/
 
 	public int getSize()
 	{
 		return size;
 	}
+	/**
+	 * gets the x coordinate of the invader
+	 * @return the x coordiante
+	 **/
 
 	public int getX()
 	{
 		return (int) location.getX();
 	}
+	/**
+	 * gets the y coordinate oft he invader
+	 * @return the y coordinate
+	 * 
+	 **/
 
 	public int getY()
 	{
 		return (int) location.getY();
 	}
+	/**
+	 * moves the invader to a given x and y coordinate. More for the creating the first position
+	 * @param x - x coordinate where the invader will move
+	 * @param y - y coordinate where the invader will move
+	 **/
 
 	public void move(int x, int y)
 	{
 		location.setX(x);
 		location.setY(y);
 	}
-
+	/**
+	 * move the invader by adding a given value to the x and y values
+	 * @param x- adds this amount to the x coordinate
+	 * @param y- adds this amount to the y coordinate
+	 **/
 	public void moveRelative(int x, int y)
 	{
 		location.setX(location.getX() + x);
 		location.setY(location.getY() + y);
 	}
+	/**
+	 * sets the health of the invader 
+	 * @param health- value of health will be set to
+	 **/
 
 	public void setHealth(int health)
 	{
 		this.health = health;
 	}
+	/**
+	 * adds the given health to the invader
+	 * @param health- amount of health added to the current health of the invader
+	 **/
 
 	public void setHealthRelative(int health)
 	{
 		this.health += health;
 	}
+	/**
+	 * sets the sive of the invader to the given size
+	 * @param size- size that the invader will be set to
+	 **/
 
 	public void setSize(int size)
 	{
 		this.size = size;
 	}
+	/**
+	 * shoots a projectile from the invader object
+	 **/
 
 	public void shoot()
 	{
