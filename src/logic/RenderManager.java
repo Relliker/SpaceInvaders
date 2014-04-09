0087539f38abf407cc838a506c0fc1e71673242e
MattYed <yedmatt@gmail.com>
2014-04-09 09:38:04 -0600

    Update RenderManager.java

--- src/logic/RenderManager.java
+++ src/logic/RenderManager.java
@@ -7,7 +7,9 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-
+/**
+ * manages rendering.  
+ */
 public class RenderManager implements Constants, CollisionMasks
 {
 
@@ -18,7 +20,10 @@ public class RenderManager implements Constants, CollisionMasks
 	private static Image scoreBackground;
 	private static Image redInvader;
 	private static Image greenInvader;
-
+	/**
+	 * draws the AI ships. Computer ships.
+	 * @param g - graphics to use.
+	 */
 	private static void drawAIShips(final Graphics g)
 	{
 		if (ShipManager.getInvaders() == null) return;
@@ -26,12 +31,22 @@ public class RenderManager implements Constants, CollisionMasks
 			current.draw(g);
 	}
 
+
+	/**
+	 * draws the backgrounds.
+	 * @param g - graphics to use.
+	 */
 	public static void drawBackgrounds(final Graphics g)
 	{
 		if (!debugMode) g.drawImage(background, 0, 0, windowX, windowY, 0, 0, windowX, windowY);
 		g.drawImage(scoreBackground, 0, 0, windowX, 40, 0, 0, windowX * 2, 160);
 	}
-
+	
+	
+	/**
+	 * draws the bar for hull strength
+	 * @param g - graphics to use.
+	 */
 	private static void drawBars(Graphics g)
 	{
 		PlayerShip ps = ShipManager.getPlayerShip();
@@ -40,20 +55,31 @@ public class RenderManager implements Constants, CollisionMasks
 		g.drawRect((float) (5 * scale), (float) (windowY - (100 * scale)), (float) ((ps.getHullStrength() / ps.getMaxHullStrength()) * (100 * scale)), (float) (25 * scale));
 		g.setColor(temp);
 	}
-
+	
+	/**
+	 * draws the player ship
+	 * @param g - graphics to use.
+	 */
 	private static void drawPlayerShips(final Graphics g)
 	{
 		ShipManager.getPlayerShip().draw(g);
 	}
 
+	/**
+	 * draws the text for the top bar.
+	 * @param g - graphics to use.
+	 */
 	private static void drawText(final Graphics g)
 	{
 		// NOTE: Each char is 8 pixels wide and 8 pixels tall
 		if (debugMode) g.drawString("Frame: " + frame + "   Wave: " + ShipManager.getWave() + "  Lives: " + ShipManager.getLives() + "       DEBUG MODE", 100, 10);
 		else g.drawString("Wave: " + ShipManager.getWave() + "  Lives: " + ShipManager.getLives(), 100, 10);
 	}
-
-	public static void loadResources(final Graphics g) throws SlickException
+		
+	/**
+	 * loads the images.
+	 */
+	public static void loadResources() throws SlickException
 	{
 		background = new Image(backgroundPath + "stars1.jpg");
 		scoreBackground = new Image(miscPath + "metalbeam2.jpg");
@@ -62,7 +88,11 @@ public class RenderManager implements Constants, CollisionMasks
 		greenInvader = new Image(shipsPath + "greenInvader.png");
 		greenInvader = greenInvader.getFlippedCopy(false, true);
 	}
-
+	
+	/**
+	 * Runs the next Render.
+	 * @param g - graphics to use.
+	 */
 	public static void nextRender(final Graphics g)
 	{
 		g.setClip(0, 0, windowX, windowY);
@@ -74,7 +104,12 @@ public class RenderManager implements Constants, CollisionMasks
 		drawPlayerShips(g);
 		frame++;
 	}
-
+	
+		
+	/**
+	 * Shows the word paused. used when game is paused
+	 * @param g - graphics to use.
+	 */
 	public static void paused(final Graphics g)
 	{
 		g.drawString("PAUSED", windowX / 2, windowY / 2);
